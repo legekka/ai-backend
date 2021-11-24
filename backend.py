6c289c54f0a912f65c699bc51bb9765a91f23d08
Dénes Márk <legekka@gmail.com>
2021-11-24 16:30:18 +0100

    Add support for model retraining

--- backend.py
+++ backend.py
@@ -1,4 +1,6 @@
 # RaterNN part
+from flask_cors import CORS
+from flask import Flask, jsonify, request
 import os
 import time
 import numpy as np
@@ -9,6 +11,14 @@ from torchvision import models
 import torch
 from torch import nn
 import json
+from torch.utils.data.dataloader import DataLoader
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import random
+import asyncio
+
+cudadevice = "cuda"
 
 # load users.json
 with open("models/users.json") as f:
@@ -26,7 +36,8 @@ class Resnext101(nn.Module):
         resnet = models.resnext101_32x8d(pretrained=False)
         resnet.fc = nn.Sequential(
             nn.Dropout(p=0.2),
-            nn.Linear(in_features=resnet.fc.in_features, out_features=n_classes),
+            nn.Linear(in_features=resnet.fc.in_features,
+                      out_features=n_classes),
         )
         self.base_model = resnet
         self.sigm = nn.Sigmoid()
@@ -41,7 +52,8 @@ class Resnext50(nn.Module):
         resnet = models.resnext50_32x4d(pretrained=False)
         resnet.fc = nn.Sequential(
             nn.Dropout(p=0.2),
-            nn.Linear(in_features=resnet.fc.in_features, out_features=n_classes),
+            nn.Linear(in_features=resnet.fc.in_features,
+                      out_features=n_classes),
         )
         self.base_model = resnet
         self.sigm = nn.Sigmoid()
@@ -70,8 +82,34 @@ class Rater(nn.Module):
 
     def forward(self, x):
         tagger_out = tagger(x)
-        tagger_out = tagger_out.reshape(tagger_out.shape[0], tagger_out.shape[1])
+        tagger_out = tagger_out.reshape(
+            tagger_out.shape[0], tagger_out.shape[1])
         return self.base_model(tagger_out)
+
+
+class RDataset(Dataset):
+    def __init__(self, training_file, img_folder, transforms):
+        self.img_folder = img_folder
+        self.transforms = transforms
+        self.data = json.load(open(training_file))
+        self.len = len(self.data)
+
+        self.imgs = []
+        self.ratings = []
+        for i in range(self.len):
+            self.imgs.append(self.data[i]['image'])
+            self.ratings.append(self.data[i]['rating'])
+
+    def __getitem__(self, index):
+        rating = self.ratings[index]
+        img_path = os.path.join(self.img_folder, self.imgs[index])
+        img = Image.open(img_path)
+        if self.transforms is not None:
+            img = self.transforms(img)
+        return img, rating
+
+    def __len__(self):
+        return self.len
 
 
 mean = [0.485, 0.456, 0.406]
@@ -84,19 +122,21 @@ val_transform = transforms.Compose(
     ]
 )
 
-device = torch.device("cuda")
+device = torch.device(cudadevice)
 
 # loading model from saved files
 print("Loading models...")
 taggernn = Resnext50(989)
-taggernn.load_state_dict(torch.load("models/TaggerNN4S.pth", map_location=device))
+taggernn.load_state_dict(torch.load(
+    "models/TaggerNN4S.pth", map_location=device))
 taggernn.to(device)
 taggernn.eval()
 print("TaggerNN4S loaded.")
 
 print("Loading TaggerNN4S(-2) for Rater")
 tagger = Resnext50(989)  # 989 classes
-tagger.load_state_dict(torch.load("models/TaggerNN4S.pth", map_location=device))
+tagger.load_state_dict(torch.load(
+    "models/TaggerNN4S.pth", map_location=device))
 tagger.eval()
 tagger.to(device)
 modules = list(list(tagger.children())[0].children())[:-1]
@@ -107,7 +147,8 @@ tagger = nn.Sequential(*modules)
 for user in users:
     print("Loading RaterNN for user: " + user)
     model = Rater()
-    model.load_state_dict(torch.load("models/" + user + ".pth", map_location=device))
+    model.load_state_dict(torch.load(
+        "models/" + user + ".pth", map_location=device))
     model.to(device)
     model.eval()
     ratermodels.append(model)
@@ -141,7 +182,8 @@ def rateImage(image, username):
     img = img.to(device)
 
     with torch.no_grad():
-        prediction = float(ratermodels[users.index(username)](img).cpu().numpy()[0][0])
+        prediction = float(ratermodels[users.index(
+            username)](img).cpu().numpy()[0][0])
     return prediction
 
 
@@ -159,8 +201,6 @@ def rateImage_all(image):
 
 
 # Web server part
-from flask import Flask, jsonify, request
-from flask_cors import CORS
 
 # configure port to 8765
 app = Flask("rater")
@@ -212,6 +252,128 @@ def ratebulk():
     return jsonify({"ratings": ratings})
 
 
+def checkfiles(data):
+    # iterate through data and check if all files exist (data.image is the filename)
+    for item in data:
+        if not os.path.isfile("images/" + item["image"]):
+            print("File " + item["image"] + " not found!")
+            return False
+    return True
+
+
+def createTrainDataFiles(data):
+    for user in users:
+        with open("training/" + user + ".json", "w") as outfile:
+            traindata = []
+            for item in data:
+                if item["username"] == user:
+                    traindata.append(
+                        {"image": item["image"], "rating": item["rating"]})
+            json.dump(traindata, outfile)
+
+
+async def trainAllUser():
+    for i, user in enumerate(users):
+        print("Training RaterNN for user " + user + +
+              " " + (i + 1) + "/" + str(len(users)))
+        await trainUser(user)
+    return
+
+
+async def trainUser(username):
+    print("AAA")
+    current_training_status["user"] = username
+    current_training_status["progress"] = 0
+    current_training_status["is_training"] = True
+    epoch = 1
+    print("Training user " + username + "...")
+    training_data = RDataset("training/" + username +
+                             ".json", "images", train_transform)
+    print("Training data loaded.")
+    train_loader = DataLoader(training_data, batch_size=batch_size,
+                              num_workers=num_workers, drop_last=True, shuffle=True)
+    model = Rater()
+    model.to(device)
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    criterion = nn.BCELoss()
+    while True:
+        batch_losses = []
+        loop = tqdm(train_loader)
+        for imgs, targets in loop:
+            imgs, targets = imgs.to(device), targets.to(device)
+            optimizer.zero_grad()
+
+            model_result = model(imgs)
+            loss = criterion(model_result, targets.type(torch.float))
+            batch_loss_value = loss.item()
+            loss.backward()
+            optimizer.step()
+
+            batch_losses.append(batch_loss_value)
+            loop.set_description(
+                f"Epoch [{epoch}/{max_epoch_number}] | Loss: {np.mean(batch_losses):.3f}")
+        current_training_status["progress"] = epoch / max_epoch_number
+        epoch += 1
+        if max_epoch_number < epoch:
+            break
+    torch.save(model.state_dict(), "models/" + username + ".pth")
+    print("User " + username + " trained, model saved!")
+    index = users.index(username)
+    ratermodels[index].load_state_dict(model.state_dict())
+    print("Model updated!")
+    current_training_status["is_training"] = False
+
+
+@app.route("/training/status", methods=["GET"])
+def trainingStatus():
+    return jsonify(current_training_status)
+
+
+@app.route("/training/train", methods=["POST"])
+def train():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    if current_training_status["is_training"]:
+        return jsonify({"error": "Training already in progress"})
+    user = request.form.get("user")
+    if (user == "all"):
+        print("Training all users")
+        loop.run_until_complete(trainAllUser())
+        return jsonify({"status": "Training all users complete"})
+
+    if user not in users:
+        print("User " + user + " not found!")
+        return jsonify({"error": "User not found"})
+    loop.run_until_complete(trainUser(user))
+    return jsonify({"success": "Training complete"})
+
+
+@app.route("/training/update", methods=["POST"])
+def updateTrainingData():
+    data = json.loads(request.form.get("data"))
+    if not checkfiles(data):
+        return jsonify({"error": "Images are missing!"})
+    createTrainDataFiles(data)
+    print("Training data updated!")
+    return jsonify({"success": "Data updated!"})
+
+
+@app.route("/training/addimages", methods=["POST"])
+def addImages():
+    images = request.files.getlist("images")
+    for image in images:
+        image.save("images/" + image.filename)
+    return jsonify({"status": "ok"})
+
+
+@app.route("/training/addimage", methods=["POST"])
+def addImage():
+    image = request.files.get("image")
+    image.save("images/" + image.filename)
+    return jsonify({"success": "Image saved"})
+
+
 @app.route("/updatemodel", methods=["POST"])
 def updatemodel():
     modelfile = request.files.get("pth")
@@ -224,13 +386,49 @@ def updatemodel():
         return jsonify({"error": "User not found"})
     # get index of user in users
     index = users.index(user)
-    ratermodels[index].load_state_dict(torch.load(modelfile, map_location=device))
+    ratermodels[index].load_state_dict(
+        torch.load(modelfile, map_location=device))
     # save modelfile into the models folder
     torch.save(ratermodels[index].state_dict(), "models/" + user + ".pth")
     print("Model for user " + user + " updated.")
     return jsonify({"success": "Model updated"})
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="2444")
+# Retrainer part
 
+# Fix all seeds to make experiments reproducible
+torch.manual_seed(2021)
+torch.cuda.manual_seed(2021)
+np.random.seed(2021)
+random.seed(2021)
+torch.backends.cudnn.deterministic = True
+
+train_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(),
+    transforms.RandomAffine(degrees=20, translate=(0.2, 0.2), scale=(0.5, 1.5),
+                            shear=None, resample=False,
+                            fillcolor=tuple(np.array(np.array(mean)*255).astype(int).tolist())),
+    transforms.ToTensor(),
+    transforms.Normalize(mean, std)
+])
+
+current_training_status = {
+    "is_training": False,
+    "user": "",
+    "progress": 0
+}
+
+# Initialize the training parameters.
+num_workers = 8  # Number of CPU processes for data preprocessing
+lr = 1e-4 * 3  # Learning rate
+batch_size = 32  # Batch size
+max_epoch_number = 20  # Max epoch number
+
+device = torch.device(cudadevice)
+
+
+# if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port="2444")
+app.run(host="0.0.0.0", port="2444")
