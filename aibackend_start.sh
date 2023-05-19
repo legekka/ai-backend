eb03458a05d6f2c28b8b35f2a21353a7c98cd019
Dénes Márk <legekka@gmail.com>
2023-05-19 08:53:33 +0200

    Base

--- aibackend_start.sh
+++ aibackend_start.sh
@@ -1,17 +1,6 @@
 #!/bin/zsh
 
-__conda_setup="$('/opt/anaconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/anaconda/etc/profile.d/conda.sh" ]; then
-        . "/opt/anaconda/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/anaconda/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
-conda activate env_pytorch
+eval "$(/home/maiia@boltz.local/miniconda3/bin/conda shell.zsh hook)"
+conda activate aibackend
 cd /opt/bots/ai-backend
 python backend.py
